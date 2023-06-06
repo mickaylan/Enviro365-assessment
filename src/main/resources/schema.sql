0271f296212533f88978a6466b43d52a10e00880
Mickaylan Hendricks <michendr021@student.wethinkcode.co.za>
2023-06-06 11:54:30 +0200

    table added and database created

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+create table AccountProfileTB (
+    id INT primary key auto_increment,
+    name varchar(255) not null,
+    surname varchar(255) not null,
+    httpImageLink varchar(255) not null
+);
