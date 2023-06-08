70371bfb1c9687696240a3f8af77317efbc808f4
Mickaylan Hendricks <michendr021@student.wethinkcode.co.za>
2023-06-08 20:06:12 +0200

    adding info to table done

--- src/main/java/com/enviro/assessment/grad001/MickaylanHendricks/data/AccountCSVData.java
+++ src/main/java/com/enviro/assessment/grad001/MickaylanHendricks/data/AccountCSVData.java
@@ -1,38 +1,48 @@
 package com.enviro.assessment.grad001.MickaylanHendricks.data;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.enviro.assessment.grad001.MickaylanHendricks.model.AccountProfile;
+import com.enviro.assessment.grad001.MickaylanHendricks.repository.AccountProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+import javax.xml.bind.DatatypeConverter;
 import java.io.*;
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import javax.xml.bind.*;
+import java.net.URISyntaxException;
 
 
+@Component
 public class AccountCSVData implements FileParser {
 
-    public AccountCSVData() {
-        AccountCSVData data = new AccountCSVData();
-        data.parseCSV(new File("src/main/resources/1672815113084-GraduateDev_AssessmentCsv_Ref003.csv"));
+    private final AccountProfileRepository accountProfileRepository;
+
+    @Autowired
+    public AccountCSVData(AccountProfileRepository accountProfileRepository) {
+        this.accountProfileRepository = accountProfileRepository;
     }
 
     @Override
     public void parseCSV(File csvFile) {
-        Set<String[]> store = new HashSet<>();
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             String line;
             br.readLine();
             while ((line = br.readLine()) != null) {
-                store.add(line.split(","));
+                String[] rowData = line.split(",");
+                String accountHolderName = rowData[0];
+                String accountHolderSurname = rowData[1];
+                String fileType = rowData[2];
+                String base64ImageData = rowData[3];
+
+                File imageFile = convertCSVDataToImage(fileType, base64ImageData);
+                URI imageLink = createImageLink(imageFile);
+
+                accountProfileRepository.save(new AccountProfile(accountHolderName, accountHolderSurname, imageLink));
+
+
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }
-
-        for (String[] a : store) {
-            convertCSVDataToImage(a[2], a[3]);
         }
     }
 
@@ -63,7 +73,11 @@ public class AccountCSVData implements FileParser {
 
     @Override
     public URI createImageLink(File fileImage) {
-        return fileImage.toURI();
+        try {
+            return new URI(fileImage.getAbsolutePath());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,7 @@
-create table AccountProfileTB (
-    id INT primary key auto_increment,
-    name varchar(255) not null,
-    surname varchar(255) not null,
-    httpImageLink varchar(255) not null
+create table ACCOUNT_PROFILETB (
+    id BIGINT auto_increment,
+    ACCOUNT_HOLDER_NAME varchar(255) not null,
+    ACCOUNT_HOLDER_SURNAME varchar(255) not null,
+    HTTP_IMAGE_LINK varchar(255) not null,
+    PRIMARY KEY (id)
 );
